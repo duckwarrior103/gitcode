0e70a7c66e1cf54ab86b9393ef34984bbb4b2ff3
Yip, Kwan Yu <133111+223136328@users.noreply.github.build.ge.com>
2024-11-28 16:55:30 +0000

    200-number-of-islands solution

--- graphs/200-Number-Of-Islands.py
+++ graphs/200-Number-Of-Islands.py
@@ -4,3 +4,25 @@ An island is surrounded by water and is formed by connecting adjacent lands hori
 You may assume all four edges of the grid are all surrounded by water.
 
 """
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        # loop through all squares, if the square is a piece of land then perform dfs and mark it as zero 
+        # after encountering a 1, dfs should flip all connected land to 0
+
+        num_islands = 0
+
+        def dfs(i, j):
+            grid[i][j] = '0'
+            for a, b in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                if 0 <= i+a < len(grid) and 0 <= j+b < len(grid[0]) and grid[i+a][j+b] == '1':
+                    dfs(i+a, j+b)
+
+        for i, row in enumerate(grid): 
+            for j, square in enumerate(row): 
+                if square   == '1':
+                    num_islands += 1
+                    dfs(i, j)
+                    
+        return num_islands                    
